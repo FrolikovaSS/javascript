65e84fe30bff7bf2fc33956b597800dc59b5eebf
Svetlana Frolikova <apples-time@yandex.ru>
2016-09-28 13:34:43 +0400

    New Project with Angular

---
+++ conFusion/app/scripts/app2.js
@@ -0,0 +1,72 @@
+'use strict';
+var app = angular.module('confusionApp', []);
+
+app.controller('confusionApp', []).controller('dishDetailController', ['$scope', function ($scope) {
+
+
+        $scope.dish = {
+            name: 'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+
+            ],
+            commentsOrderBy: ''
+        };
+
+    }])
+    .controller('FeedbackController', ['$scope', function ($scope) {
+
+        function Feedback() {
+        }
+
+        Feedback.prototype = {
+            rating: "5",
+            comment: "",
+            author: "",
+            date: ""
+        };
+
+        $scope.feedback = new Feedback();
+
+        $scope.sendFeedback = function () {
+            $scope.feedback.date = new Date();
+            $scope.dish.comments.push($scope.feedback);
+            $scope.feedback = new Feedback();
+            $scope.feedbackForm.$setPristine();
+        }
+    }]);
+
